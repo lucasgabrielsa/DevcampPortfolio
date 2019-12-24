f78020f71df2d2032d0a200ff89cbdc7cb4406f2
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2019-12-24 10:56:55 -0300

    index new create - portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,33 @@
 class PortfoliosController < ApplicationController
-
-    def index 
+    # GET /porfolios
+    def index
         @portfolio_items = Portfolio.all
     end
+
+    # GET /portfolios/new
+    def new
+        @portfolio_item = Portfolio.new
+    end
+
+    # POST /portfolios
+    def create
+        @portfolio_item = Portfolio.new(portfolio_params)
+    
+        respond_to do |format|
+          if @portfolio_item.save
+            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }            
+          else
+            format.html { render :new }
+          end
+        end
+    end
+
+
+
+    private 
+    # Never trust parameters from the scary internet, only allow the white list through.
+      def portfolio_params
+        params.require(:portfolio).permit(:title, :subtitle, :body)
+      end
 
 end
