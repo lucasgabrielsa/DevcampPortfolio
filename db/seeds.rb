fb8a52ec1425ca29791ecf6be99776dcafd1eb10
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2019-12-24 10:11:08 -0300

    add seed.rb to rails db:setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |count|
+    Blog.create!(
+        title: "My Blog Post #{count}",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |count| 
+    Skill.create!(
+        title: "Rails #{count}",
+        percent: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |count|
+    Portfolio.create!(
+        title: "Portlfolio #{count}",
+        subtitle: "My great service",
+        body: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
+        main_image: "https://via.placeholder.com/600x400",
+        thumb_image: "https://via.placeholder.com/350x200"
+    )
+end
+
+puts "9 portfolio items created"
