f65ca3368b0a795b8427ac7c0558f2f6b27e0453
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2019-12-26 10:34:10 -0300

    friendly id - permalinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :portfolios, :blogs
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as:'portfolio_show'
+
+
+  resources :blogs
 
   root to: 'pages#home'
   #get 'pages/home'
