8513e1505d10c85d7377fef998900f1aa752bc9c
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2019-12-26 09:45:02 -0300

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  resources :blogs
+  resources :portfolios, :blogs
+
+  root to: 'pages#home'
+  #get 'pages/home'
+
+  #get 'pages/about
+  get 'about', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
+  #get 'pages/contact'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
