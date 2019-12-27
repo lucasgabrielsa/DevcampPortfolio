54b869e080e7f0bc619855a2a6afd657ee0b2cbb
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2019-12-27 11:03:33 -0300

    Conncerns how to use

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,23 @@
 class Portfolio < ApplicationRecord
-
+    include Placeholder
     validates_presence_of(:title, :body, :main_image, :thumb_image) 
 
+    #same way to do the same thing
+    #same way to do the same thing
     def self.angular
-        where(title: "Portlfolio Angular")
+        where(title: 'Portlfolio Angular')
     end
 
-    scope :angular_scope, -> { where(title: "Portlfolio Angular") }
+    scope :angular_scope, -> { where(title: 'Portlfolio Angular') }
+    ######################################
+    ######################################
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        # self.main_image ||= 'https://via.placeholder.com/600x400'
+        # self.thumb_image ||= 'https://via.placeholder.com/350x200'
+        self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+        self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
+    end
 end
