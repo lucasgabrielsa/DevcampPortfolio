aa0e0ef47573cfe97dc17413f27c8975a84b63be
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2019-12-30 17:44:07 -0300

    Configuring Devise Parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: { 'sign_in': 'login', 'sign_out': 'logou', sign_up:'register'}
+  devise_for :users, path_names: { 'sign_in': 'login', 'sign_out': 'logout', sign_up:'register'}
   resources :policenas
 
   get  'test-policena', to: 'policenas#index', as: 'samuca'
