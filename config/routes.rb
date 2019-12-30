457f3e0448bef39d1c6195d89b0a4f87d1ad00bd
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2019-12-30 16:28:38 -0300

    Devise implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path_names: { 'sign_in': 'login', 'sign_out': 'logou', sign_up:'register'}
   resources :policenas
 
   get  'test-policena', to: 'policenas#index', as: 'samuca'
