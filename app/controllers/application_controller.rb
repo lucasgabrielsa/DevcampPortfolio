847da04080779b9f33cd15974126c2221b90358f
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-01-08 18:38:30 -0300

    Module Example

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
     include CurrentUserConcern
     include DefaultPageContent
 
+    before_action :set_copyright
+
     # def current_user
     #     super || OpenStruct.new(first_name:'Guest User', last_name: '', email:'guest@g1.com')
     # end
@@ -23,4 +25,17 @@ class ApplicationController < ActionController::Base
     #     devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
     #     devise_parameter_sanitizer.permit(:account_update, keys:[:name])        
     # end
+
+    def set_copyright
+        @copyright = DevcampViewTool::Renderer.copyright 'Lucas Gabriel', 'BTL Technology'
+    end
+
 end
+
+module DevcampViewTool
+    class Renderer
+      def self.copyright name, msg
+        "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      end
+    end
+  end
