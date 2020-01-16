a9c635bd035ab55b00a9771a1534cad88d2a5f75
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2020-01-16 09:14:02 -0300

    Portolio styles update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
 
   def login_helper(classe='')
     if current_user.is_a?(GuestUser)
-      (link_to "Login", new_user_session_path, class: classe).html_safe +
+      (link_to "Login", new_user_session_path, class: classe).html_safe + " " +
       (link_to "Register", new_user_registration_path, class: classe)      
     else
        (link_to "Logout", destroy_user_session_path, method: :delete, class: classe).html_safe
